8a3835d5b23601bdc6cd4873b5d80e86cb92aaf0
koudyt <info@digitalniweb.cz>
2023-03-22 10:55:32 +0100

    registerCurrentApp

--- plugins/serverInit.ts
+++ plugins/serverInit.ts
@@ -1,14 +1,14 @@
 import Subscriber from "./../digitalniweb-custom/helpers/subscriberService.js";
 import {
-	registerCurrentService,
+	registerCurrentMicroservice,
 	requestServiceRegistryInfo,
+	registerCurrentApp,
 } from "./../digitalniweb-custom/helpers/serviceRegistryCache.js";
 
 export default defineNuxtPlugin(async (nuxtApp) => {
-	await Subscriber.psubscribe("serviceRegistry-responseInformation-*"); // handled in registerCurrentService()
+	/* await Subscriber.psubscribe("serviceRegistry-responseInformation-*"); // handled in registerCurrentService()
 	let serviceRegistryInfo = await requestServiceRegistryInfo();
 	if (!serviceRegistryInfo)
-		throw new Error("Couldn't get serviceRegistry information.");
-
-	await registerCurrentService();
+		throw new Error("Couldn't get serviceRegistry information."); */
+	// await registerCurrentApp();
 });
