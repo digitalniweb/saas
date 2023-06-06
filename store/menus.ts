7f76ff2c42d5950de73847db2494b22a0c7ad707
koudyt <digitalniweb@gmail.com>
2023-06-06 17:20:11 +0200

    packages updated

--- store/menus.ts
+++ store/menus.ts
@@ -2,13 +2,13 @@ import { defineStore } from "pinia";
 
 export const useMenus = defineStore("menus", {
 	state: () => ({
-		articles: {}, // main menu with articles
+		articles: [] as object[], // main menu with articles (add and change type in here for menu)
 	}),
 	getters: {},
 	actions: {
 		async loadData() {
 			let articlesmenu = await useFetch("/api/website/menu");
-			this.articles = articlesmenu.data.value ?? {};
+			this.articles = articlesmenu.data.value ?? [];
 		},
 	},
 });
