5af0ac1763d7f7bf282f72da1c8dd7c929bed757
koudyt <digitalniweb@gmail.com>
2023-07-24 14:13:54 +0200

    database plugin (not wirking) removed

--- server/api/app/languages.get.ts
+++ server/api/app/languages.get.ts
@@ -1,11 +1,6 @@
 import AppLanguage from "../../models/apps/appLanguage";
 export default eventHandler(async (event) => {
 	try {
-		/* console.log(
-			"event.context.db.AppLanguage",
-			event.context.db.AppLanguage
-		);
-		return []; */
 		let languages = await AppLanguage.findAll();
 
 		return languages;
--- server/plugins/database.ts
+++
@@ -1,7 +0,0 @@
-// import AppLanguage from "../models/apps/appLanguage";
-
-export default defineNitroPlugin((nitroApp) => {
-	// console.log("event.context", nitroApp);
-	/* event.context.db = {};
-	event.context.db.AppLanguage = AppLanguage; */
-});
